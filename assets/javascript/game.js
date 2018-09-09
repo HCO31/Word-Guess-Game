3fe2c8b438c894c3c539e992a8642c56c5903068
Hernan <hernancoviedo94@gmail.com>
2018-09-08 20:50:06 -0700

    <COMMENT>

---
+++ assets/javascript/game.js
@@ -0,0 +1,93 @@
+var wordBank = ['red', 'blue'];
+var wins = 0;
+var loss = 0;
+var wrongLetter = []; //
+var guessesLeft = 8;
+var underScores = [];
+var userGuesses = [];
+var randWord;
+var winCounter = 0;
+
+// Function 
+function startGame(){
+    // picks random word
+    randWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+    for(var i = 0; i < randWord.length; i++)
+    {
+        underScores.push('_');
+
+    }
+        // printing underscores to the screen
+        document.getElementById('word-blanks').textContent = underScores.join(' ');
+
+        // reset
+        function reset(){
+        wrongLetter = [];
+        guessesLeft = 3;
+        winCounter = 0;
+        startGame()
+        }
+        
+
+        // HTML
+        document.getElementById('guesses-left').textContent = guessesLeft;
+        document.getElementById('wins').textContent = wins;
+        document.getElementById('losses').textContent = loss;
+        document.getElementById('wrong-guesses').textContent = wrongLetter;
+        
+        
+
+}
+
+
+
+
+// alert and add win or loss
+function winlose()
+{
+    if(winCounter === randWord.length)
+    {
+        wins++;
+        alert('You Win!');
+        startGame()
+    }
+    else if(guessesLeft === 0)
+    {
+        loss++;
+        alert('You Lose!');
+        startGame()
+    }
+}
+
+// key event for userGuesses
+document.onkeyup = function(event)
+{
+    userGuesses = event.key;
+    // checking if the letter exist inside of the word
+    if(randWord.indexOf(userGuesses) > -1)
+    {
+        for(var i = 0; i < randWord.length; i++)
+        {
+            if(randWord[i] === userGuesses)
+            {
+                underScores[i] = randWord[i];
+                winCounter++;
+                winlose();
+            }
+        }
+    }
+    else
+    {
+        wrongLetter.push(userGuesses)
+        guessesLeft--;
+        winlose()
+        console.log(underScores);
+    }
+}
+
+
+// Main 
+// ********************************
+
+startGame();
